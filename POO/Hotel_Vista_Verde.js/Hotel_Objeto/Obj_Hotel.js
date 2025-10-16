8510ded9ce4ebad619fb987143b8a67d377a888f
jacielcastro <jacielcastro100@gmail.com>
2025-10-16 02:55:12 -0300

    :books: -- Atualização --

--- POO/Hotel_Vista_Verde.js/Hotel_Objeto/Obj_Hotel.js
+++ POO/Hotel_Vista_Verde.js/Hotel_Objeto/Obj_Hotel.js
@@ -3,17 +3,20 @@ import { Hotel } from "../Hotel_Classes/Classes_Hotel.js";
 const Prompt = PromptSync()
 
 
-console.log(`  ===== SEJA BEM VINDO AO HOTEL VISTA VERDE =====
+console.log(`  \n===== SEJA BEM VINDO AO HOTEL VISTA VERDE =====
     ==== FAÇA A SUA RESERVA EM NOSSO HOTEL ====
+        
         ATENDENTE DO NOSSO HOTEL DIGITI (1)
         SE FOR CLIENTE DIGITI (2)`);
 
     
 let hotel = new Hotel ()
 hotel.adicionarQuarto(30,"solteiro")
-// hotel.listarQuartoDisponiveis()
+hotel.adicionarQuarto(25,"solteiro")
 hotel.adicionarQuarto(15,"familia")
+
 hotel.listarQuartoDisponiveis()
 
-hotel.reservaQuarto(25,"20/10/2025","Jaciel Castro") // ESTOU ADICIONANDO O UMA RESERVA NO HOTEL, ATRAVES DE UM METODO NA CLASSE HOTEL
-// res_cliente.listarTodasReservas()
+hotel.reservaQuarto(30,"solteiro","30/10/2025","Juliana Karoline")// ESTOU ADICIONANDO O UMA RESERVA NO HOTEL, ATRAVES DE UM METODO NA CLASSE HOTEL
+//hotel.reservaQuarto(25,"solteiro")
+hotel.listaTodasReservas()
